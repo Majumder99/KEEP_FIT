1231dc29f512d8632d62789e08253e9d19b931ef
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-07-22 22:21:15 +0600

    added basic by sourav

---
+++ app/src/main/java/com/example/keep_fit/UserDetails.java
@@ -0,0 +1,14 @@
+package com.example.keep_fit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_details);
+    }
+}
